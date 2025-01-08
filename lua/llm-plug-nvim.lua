591da1080d5aba5900d4b10fb54aff97953a3af5
egordigitax <fantomaka@gmail.com>
2025-01-08 14:56:50 +0500

    changes

--- lua/llm-plug-nvim.lua
+++ lua/llm-plug-nvim.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.setup()
-  vim.api.nvim_set_keymap('n', '<Leader>gl', ":lua print('Plugin initialized and keybinding works!')<CR>")
+  vim.api.nvim_set_keymap('n', '<Leader>gl', ":lua print('Plugin initialized and keybinding works!')<CR>", { noremap = true, silent = false })
 end
 
 return M
